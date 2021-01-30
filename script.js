d4d4c34fcf587d92ee791efa4c1176447ab62a2c
Anastasia Shimko <anastasiaishimko@gmail.com>
2021-01-30 13:50:55 +0300

    Refactoring

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var defaultColor = 'white';
 
-document.getElementById("1").onmouseover = function () { changeColor('yellow'); }
-document.getElementById("2").onmouseover = function () { changeColor('blue'); }
-document.getElementById("3").onmouseover = function () { changeColor('green'); }
+document.getElementById("1").onmouseover = function() { changeColor('yellow'); }
+document.getElementById("2").onmouseover = function() { changeColor('blue'); }
+document.getElementById("3").onmouseover = function() { changeColor('green'); }
 
-document.getElementById("1").onmouseleave = function () { changeColor(defaultColor); }
-document.getElementById("2").onmouseleave = function () { changeColor(defaultColor); }
-document.getElementById("3").onmouseleave = function () { changeColor(defaultColor); }
+document.getElementById("1").onmouseleave = function() { changeColor(defaultColor); }
+document.getElementById("2").onmouseleave = function() { changeColor(defaultColor); }
+document.getElementById("3").onmouseleave = function() { changeColor(defaultColor); }
 
 function changeColor(color) {
     document.body.style.background = color;
@@ -15,4 +15,4 @@ function changeColor(color) {
 function onRectangleClick(color) {
     document.body.style.background = color;
     defaultColor = color;
-} 
+}
